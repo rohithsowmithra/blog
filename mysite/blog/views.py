393a749cea7daa7c5892ed4f86f0e8eddc60a772
rohithsowmithra <rohithsowmithra@gmail.com>
2021-05-23 13:50:38 +0530

    adding post_share view for email functionality to share posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,38 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
+
+def post_share(request, post_id):
+    # retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == "POST":
+        # form was submitted
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [{cd['to']}])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    context_obj = {'post': post,
+                   'form': form,
+                   'sent': sent}
+    return render(request, 'blog/post/share.html', context_obj)
+
 
 def post_list(request):
     object_list = Post.objects.all()
