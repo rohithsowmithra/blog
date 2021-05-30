0c359297db8b3cc3438cf4965dabe5fcad515337
rohithsowmithra <rohithsowmithra@gmail.com>
2021-05-30 14:05:52 +0530

    added url for filtering blog posts by tag

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,8 +5,9 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    #path('', views.post_list, name='post_list'),
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    #path('', views.PostListView.as_view(), name='post_list'),
+    path('tags/<slug:tag_slug>', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
         name='post_detail'),
