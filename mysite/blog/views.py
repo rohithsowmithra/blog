c4bee4490ebfa467acc8cdc7753d556d987f17c1
rohithsowmithra <rohithsowmithra@gmail.com>
2021-05-30 14:05:00 +0530

    add filter by tag functionality in post_list view and add comments for all ther views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,21 +4,27 @@ from .models import Post, Comment
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-# Create your views here.
+# view for sharing posts via email
 
 def post_share(request, post_id):
     # retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
 
+    # check if request is POST and if yes, send email
     if request.method == "POST":
         # form was submitted
         form = EmailPostForm(request.POST)
 
+        # check if form is valid
         if form.is_valid():
+
             # form fields passed validation
             cd = form.cleaned_data
+
+            # build post url
             post_url = request.build_absolute_uri(
                 post.get_absolute_url()
             )
@@ -26,7 +32,10 @@ def post_share(request, post_id):
             message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
             send_mail(subject, message, 'admin@myblog.com', [{cd['to']}])
             sent = True
+
+    # In case of GET request
     else:
+        # just show the empty form to the user
         form = EmailPostForm()
 
     context_obj = {'post': post,
@@ -35,31 +44,56 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', context_obj)
 
 
-def post_list(request):
+# view to show up list view of posts
+
+def post_list(request, tag_slug=None):
+    # get all Post objects
     object_list = Post.objects.all()
+
+    tag = None
+
+    # check if a tag_slug is sent as an argument and filter the posts by tag
+    if tag_slug:
+        # get the tag from Tag model based on slug
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        # filter the Post objects with the tag argument sent
+        object_list = object_list.filter(tags__in=[tag])
+
+    # Paginate the objects into chunks of 3
     paginator = Paginator(object_list, 3)
+    # get 'page' argument from GET request
     page = request.GET.get('page')
+    # if page arg is not in GET req, set it to 1
     if not page:
         page = 1
     try:
+        # get specific page
         posts = paginator.page(page)
     except PageNotAnInteger:
+        # how first page if page is not a number
         posts = Paginator.page(1)
     except EmptyPage:
+        # show last page if page number is more than max pages available
         posts = Paginator.page(paginator.num_pages)
 
     return render(request, 'blog/post/list.html', {'page': page,
-                                                   'posts': posts})
+                                                   'posts': posts,
+                                                   'tag': tag })
+
+
+# view to show up all details about a Post
 
 def post_detail(request, year, month, day, post):
+    # get Post object based on argument passed
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,\
                              publish__month=month, publish__day=day)
 
-    # List of active comments for this post
+    # List of active comments for this post (related_name field in Comment model)
     comments = post.comments.filter(active=True)
 
     new_comment = None
 
+    # check if request is POST and if yes, save the comment based on form data
     if request.method == 'POST':
         # A comment was posted
         comment_form = CommentForm(data=request.POST)
@@ -70,6 +104,8 @@ def post_detail(request, year, month, day, post):
             new_comment.post = post
             # Save the comment to the database
             new_comment.save()
+
+    # if request was GET, show up the comment form
     else:
         comment_form = CommentForm()
 
@@ -80,6 +116,8 @@ def post_detail(request, year, month, day, post):
 
     return render(request, 'blog/post/detail.html', context_object)
 
+
+# Class Based View (alternative for post_list view)
 
 class PostListView(ListView):
     queryset = Post.published.all()
